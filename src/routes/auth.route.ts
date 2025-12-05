026ff861517e2c901521f73b98e6eac88e0be3e2
amiminn <tholabul.amin@gmail.com>
2025-12-05 08:32:41 +0700

    fix: validator

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,15 +6,15 @@ export const authRoute = new Elysia({ prefix: "/auth" })
 
   .post("/login", AuthController.login, {
     body: t.Object({
-      username: t.String().trim().notEmpty(),
-      password: t.String().trim().notEmpty(),
+      username: t.String({ minLength: 1 }),
+      password: t.String({ minLength: 1 }),
     }),
   })
   .post("/register", AuthController.register, {
     body: t.Object({
-      username: t.String().trim().notEmpty(),
-      password: t.String().trim().notEmpty(),
-      email: t.String().trim().notEmpty(),
+      username: t.String({ minLength: 1 }),
+      password: t.String({ minLength: 1 }),
+      email: t.String({ minLength: 1 }),
       alamat: t.Optional(t.String()),
     }),
   })
@@ -22,7 +22,7 @@ export const authRoute = new Elysia({ prefix: "/auth" })
   .put("/profile", AuthController.updateprodfile, {
     beforeHandle: authMiddleware,
     body: t.Object({
-      email: t.String().trim().notEmpty(),
+      email: t.String({ minLength: 1 }),
       alamat: t.Optional(t.String()),
     }),
   });
